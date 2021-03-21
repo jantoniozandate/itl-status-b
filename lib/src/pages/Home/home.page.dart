44c2439b5f25222a710e958569282a0dd173d8b5
Antonio Zandate <joseantonio.zandate@gmail.com>
2021-03-21 17:20:29 -0600

    setup home page

---
+++ lib/src/pages/Home/home.page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import './components/body.home.dart';
+
+class LoginPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: BodyHome(),);
+  }
+}
