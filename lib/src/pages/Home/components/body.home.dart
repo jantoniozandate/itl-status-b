e641ad3f392174aafb4b424a43bd77ed8ac7d45e
Antonio Zandate <joseantonio.zandate@gmail.com>
2021-03-27 16:02:15 -0600

    added: current user info

--- lib/src/pages/Home/components/body.home.dart
+++ lib/src/pages/Home/components/body.home.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:itlstatusb/locator.dart';
+import 'package:itlstatusb/src/services/authentication_service.dart';
 
-class BodyHome extends StatelessWidget {
-  const BodyHome ({Key key}) : super(key: key);
+class BodyHome extends StatefulWidget {
+  const BodyHome({Key key}) : super(key: key);
+
+  @override
+  _BodyHomeState createState() => _BodyHomeState();
+}
+
+class _BodyHomeState extends State<BodyHome> {
+  final AuthenticationService _authenticationService =
+      locator<AuthenticationService>();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: Text('Home'),
-    );
+    return WillPopScope(
+        onWillPop: () async {
+          SystemNavigator.pop();
+          return false;
+        },
+        child: Container(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Home'),
+                Text(
+                    'Bienvenido ' + _authenticationService.currentUser.username)
+              ],
+            )));
   }
 }
