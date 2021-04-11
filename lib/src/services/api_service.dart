8a5a69de11639717b5f5023f3f4013bd0fcda332
Antonio Zandate <joseantonio.zandate@gmail.com>
2021-04-10 22:42:01 -0500

    added: get status from api

--- lib/src/services/api_service.dart
+++ lib/src/services/api_service.dart
@@ -1,16 +1,35 @@
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 import 'package:itlstatusb/src/models/user.dart';
+import 'package:itlstatusb/src/models/status_model.dart';
 import 'package:itlstatusb/src/config/config.dart';
 
-Future<User> doLogin(email, password) async {
-  var result = await http.post(loginUrl, body: {
-    'login': email,
-    'password': password
-  }, headers: {'X-API': xAPIKey});
-  if (result.statusCode >= 300) return null;
-  var jsonResponse = convert.jsonDecode(result.body)['data'];
-  User user = User.fromData(jsonResponse);
-  return user;
-  // return result;
+class APIService {
+  static Future<User> doLogin(email, password) async {
+    var result = await http.post(loginUrl,
+        body: {'login': email, 'password': password},
+        headers: {'X-API': xAPIKey});
+    if (result.statusCode >= 300) return null;
+    var jsonResponse = convert.jsonDecode(result.body)['data'];
+    jsonResponse['expiresAt'] = DateTime.now().toUtc().millisecondsSinceEpoch;
+    User user = User.fromData(jsonResponse);
+    return user;
+    // return result;
+  }
+
+  static Future<Status> getStatus(User user) async {
+    Map<String, String> queryParams = {
+      "id": user.id,
+      "sessionId": user.sessionId
+    };
+    String queryString = getCargaURL + '?' + Uri(queryParameters: queryParams).query;
+    var result = await http
+        .get(queryString, headers: {'X-API': xAPIKey});
+
+    if (result.statusCode >= 300) return null;
+    var jsonResponse = convert.jsonDecode(result.body)['data'];
+
+    Status status = Status.fromData(jsonResponse);
+    return status;
+  }
 }
