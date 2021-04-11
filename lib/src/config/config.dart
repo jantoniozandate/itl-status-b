d396391d5bb4b9270a6efd66d754b09e653b19f9
Antonio Zandate <joseantonio.zandate@gmail.com>
2021-04-10 22:42:30 -0500

    added: status url

--- lib/src/config/config.dart
+++ lib/src/config/config.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
 final String loginUrl = DotEnv.env['LOGIN_URL'];
+final String getCargaURL = DotEnv.env['CARGA_URL'];
 final String xAPIKey = DotEnv.env['API_KEY'];
