51f1900b45a08b5e9f85189d71a7a21f8445c84c
Antonio Zandate <joseantonio.zandate@gmail.com>
2021-04-10 22:42:15 -0500

    added: api service to locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -6,6 +6,7 @@ import 'package:itlstatusb/src/providers/push_notifications.provider.dart';
 import 'package:get_it/get_it.dart';
 import 'package:itlstatusb/src/services/navigation_service.dart';
 import 'package:itlstatusb/src/services/dialog_service.dart';
+import 'package:itlstatusb/src/services/api_service.dart';
 
 GetIt locator = GetIt.instance;
 
@@ -13,6 +14,7 @@ void setupLocator() {
   locator.registerLazySingleton(() => NavigationService());
   locator.registerLazySingleton(() => DialogService());
   locator.registerLazySingleton(() => AuthenticationService());
+  locator.registerLazySingleton(() => APIService());
   // locator.registerLazySingleton(() => FirestoreService());
   // locator.registerLazySingleton(() => CloudStorageService());
   // locator.registerLazySingleton(() => ImageSelector());
