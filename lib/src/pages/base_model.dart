4324fb0085fa59f712f5669826fc31cc5049c61b
Antonio Zandate <joseantonio.zandate@gmail.com>
2021-05-02 15:58:23 -0500

    fixed basemodel user

--- lib/src/pages/base_model.dart
+++ lib/src/pages/base_model.dart
@@ -2,12 +2,13 @@ import 'package:itlstatusb/locator.dart';
 import 'package:itlstatusb/src/models/user.dart';
 // import 'package:itlstatusb/src/services/authentication_service.dart';
 import 'package:flutter/widgets.dart';
+import 'package:itlstatusb/src/services/authentication_service.dart';
 
 class BaseModel extends ChangeNotifier {
-  // final AuthenticationService _authenticationService =
-  //     locator<AuthenticationService>();
-  User antonio = new User(email: 'antonio', fullName: 'Antonio Z', id: '1234');
-  User get currentUser => antonio; //_authenticationService.currentUser;
+  final AuthenticationService _authenticationService =
+      locator<AuthenticationService>();
+  // User antonio = new User(email: 'antonio', id: '1234');
+  User get currentUser => _authenticationService.currentUser;
 
   bool _busy = false;
   bool get busy => _busy;
