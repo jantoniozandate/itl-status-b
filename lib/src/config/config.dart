4a2520ad76e4ccab068f9db8f32fc9e1722b4d3e
Antonio Zandate <joseantonio.zandate@gmail.com>
2021-05-02 13:52:21 -0500

    account update urls

--- lib/src/config/config.dart
+++ lib/src/config/config.dart
@@ -4,3 +4,5 @@ final String loginUrl = DotEnv.env['LOGIN_URL'];
 final String registerUrl = DotEnv.env['REGISTER_URL'];
 final String getCargaURL = DotEnv.env['CARGA_URL'];
 final String xAPIKey = DotEnv.env['API_KEY'];
+final String updateUserUrl = DotEnv.env['UPDATE_USER_URL'];
+final String updateStatusUrl = DotEnv.env['UPDATE_STATUS_URL'];
