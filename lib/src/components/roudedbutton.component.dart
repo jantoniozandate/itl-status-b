dcc6f21429dfc240d260b8306b3d189bff147c94
Antonio Zandate <joseantonio.zandate@gmail.com>
2021-05-02 13:51:46 -0500

    updated styles

--- lib/src/components/roudedbutton.component.dart
+++ lib/src/components/roudedbutton.component.dart
@@ -4,11 +4,13 @@ class RoundedButton extends StatelessWidget {
   final String text;
   final Function press;
   final Color color, textColor;
+  final bool disabled;
   const RoundedButton(
       {Key key,
       this.text,
       this.press,
       this.color = Colors.blue,
+      this.disabled = false,
       this.textColor = Colors.white})
       : super(key: key);
 
@@ -18,15 +20,21 @@ class RoundedButton extends StatelessWidget {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10),
       width: size.width * 0.8,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(29),
-        child: FlatButton(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-          color: this.color,
-          onPressed: this.press,
+      child: /* ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: */ TextButton(
+          style: TextButton.styleFrom(
+            backgroundColor: this.color,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            padding: EdgeInsets.all(20),
+            elevation: 2,
+            shadowColor: this.color,
+            // side: BorderSide(color: Colors.white, width: 1)
+          ),
+          onPressed: this.disabled ? null : this.press,
           child: Text(this.text, style: TextStyle(color: this.textColor)),
         ),
-      ),
+      // ),
     );
   }
 }
